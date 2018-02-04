dc8f94a816d33397caf6c70fff45c7c86b31eee0
Rob Waller <r.t.waller@gmail.com>
2018-02-04 10:27:59 -0600

    Completed videos 1-5

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
+//  Vue instance
+
 new Vue ({
     el: '#vue-app',
+    //  Vue Data
     data: {
-        name: 'Rob'
+        name: 'Rob',
+        job: 'Boss',
+        website: 'http://www.14-10.com',
+        websiteTag: '<a href="http://www.14-10.com">My website</a>',
+        age: 28,
+        x: 0,
+        y: 0
+    },
+    //  Vue Methods: Can hold functions
+    methods: {
+        greet: function(time) {
+            // this refers to this vue instance
+            this.name
+            return 'Good ' + time + ' ' + this.name;
+        },
+        add: function(inc) {
+            this.age += inc;
+        },
+        subtract: function(dec) {
+            this.age -= dec;
+        },
+        // The event parameter is automatically passed in from DOM events
+        updateXY: function(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
     }
 });
