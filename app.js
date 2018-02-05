b2145cddf41efde5f0988d23fb1c3f90a53e53c9
Rob Waller <r.t.waller@gmail.com>
2018-02-05 16:41:12 -0600

    Finished up to video 10

--- app.js
+++ app.js
@@ -9,8 +9,15 @@ new Vue ({
         website: 'http://www.14-10.com',
         websiteTag: '<a href="http://www.14-10.com">My website</a>',
         age: 28,
+        ageBinding: '',
+        nameBinding: '',
+        ageExample2: 28,
         x: 0,
-        y: 0
+        y: 0,
+        a: 0,
+        b: 0,
+        available: false,
+        nearby: false
     },
     //  Vue Methods: Can hold functions
     methods: {
@@ -29,6 +36,36 @@ new Vue ({
         updateXY: function(event) {
             this.x = event.offsetX;
             this.y = event.offsetY;
+        },
+        addExample2: function(inc) {
+            this.ageExample2 += inc;
+        },
+        subtractExample2: function(dec) {
+            this.ageExample2 -= dec;
+        },
+        click: function() {
+            alert('You clicked me');
+        },
+        logName: function() {
+            console.log('you entered your name')
+        },
+        logAge: function() {
+            console.log('you entered your age')
+        }
+    },
+    // Different than methods. Lookup documentation to understand better
+    computed: {
+        addToA: function() {
+            return this.a + this.age
+        },
+        addToB: function() {
+            return this.b + this.age
+        },
+        compClasses: function() {
+            return {
+                available: this.available,
+                nearby: this.nearby
+            }
         }
     }
 });
